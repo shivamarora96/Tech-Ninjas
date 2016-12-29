bc341ab01b1b55540c93379274c4e71b40719bed
Shivam Arora <shivam_arora@live.in>
2016-12-29 10:56:05 +0530

    Basic Settings
    Basic Settings

---
+++ app/src/main/java/com/teamninjas/tech_ninjas/Main.java
@@ -0,0 +1,13 @@
+package com.teamninjas.tech_ninjas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
