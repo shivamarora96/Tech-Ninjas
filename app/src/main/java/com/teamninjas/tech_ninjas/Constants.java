82b6da8f0ec40ca82f8770efe9e19ed960721734
Shivam Arora <shivam_arora@live.in>
2016-12-29 13:37:53 +0530

    basic commit

---
+++ app/src/main/java/com/teamninjas/tech_ninjas/Constants.java
@@ -0,0 +1,15 @@
+package com.teamninjas.tech_ninjas;
+
+/**
+ * Created by ShivamArora on 29-12-2016.
+ */
+
+public class Constants {
+
+    final static public String SERVER_BASEURL = "" ;
+    
+
+
+
+
+}
--- app/src/main/java/com/teamninjas/tech_ninjas/Main.java
+++ app/src/main/java/com/teamninjas/tech_ninjas/Main.java
@@ -1,7 +1,10 @@
 package com.teamninjas.tech_ninjas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Main extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        FirebaseAuth.getInstance().signOut();
+        Intent i = new Intent(Main.this , LoginActivity.class);
+        startActivity(i);
     }
 }
